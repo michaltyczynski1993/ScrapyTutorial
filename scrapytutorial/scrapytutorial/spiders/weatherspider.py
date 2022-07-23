ffe74216d473b333d5bd65d303ead7ca5d90f6fe
Michal Tyczynski <m.tyczynski1993@gmail.com>
2022-07-23 18:48:34 +0200

    cleaning weather spider's code

--- scrapytutorial/scrapytutorial/spiders/weatherspider.py
+++ scrapytutorial/scrapytutorial/spiders/weatherspider.py
@@ -1,5 +1,5 @@
-from ast import parse
 import scrapy
+
 
 class WeatherScraper(scrapy.Spider):
     name = 'weather_spider'
@@ -7,12 +7,16 @@ class WeatherScraper(scrapy.Spider):
 
     def parse(self, response):
 
-        current_weather = response.xpath('//*[@class = "weather-currently-temp-strict"]/text()').get()
-        current_city = response.xpath('//h3[@class = "weather-currently-city"]/text()').get()
-        perceptible = response.xpath('//*[@class = "weather-currently-details-value"]/text()').get()
-        pressure = response.xpath('//*[@class = "weather-currently-details-value steady"]/text()').get()
-        wind = response.xpath('//*[@class = "weather-currently-details-item wind"]/span/text()').get()
-        # try:
+        current_weather = response.xpath(
+            '//*[@class = "weather-currently-temp-strict"]/text()').get()
+        current_city = response.xpath(
+            '//h3[@class = "weather-currently-city"]/text()').get()
+        perceptible = response.xpath(
+            '//*[@class = "weather-currently-details-value"]/text()').get()
+        pressure = response.xpath(
+            '//*[@class = "weather-currently-details-value steady"]/text()').get()
+        wind = response.xpath(
+            '//*[@class = "weather-currently-details-item wind"]/span/text()').get()
         yield{
             'city': current_city.strip(),
             'weather': current_weather.strip(),
@@ -20,12 +24,3 @@ class WeatherScraper(scrapy.Spider):
             'atmospheric pressure': pressure.strip(),
             'wind': wind.strip()
         }
-        # except:
-        #     yield{
-        #         'city': current_city,
-        #         'weather': current_weather,
-        #         'perceptible temperature': perceptible,
-        #         'atmospheric pressure': pressure,
-        #         'wind': wind.strip()
-        #     }
-
